354ed216bff40dcceea405f8be047182b468159a
SHUBERTH CORDERO <128418334+shuberth79@users.noreply.github.com>
2025-06-06 17:38:56 +0200

    dotenv, session, bcrypt, ejs, carpeta public

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //9 iniciar las librerias
 const express = require ("express")
 const app = express();
-
+require = ("dotenv").config({path:"./env/.env"});
+const bcrypt = require ("bcrypt.js");
+const session = require ("express-session");
 
 //9.2 crear el servidor
 app.listen(3000, () => {  //
@@ -17,3 +19,19 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+// 9.5 Configurar carpeta publica o estática
+app.use('/resources', express.static(__dirname + '/public'));
+
+//9.6 Definir el motor de vistas
+app.set("view engine", "ejs");
+app.set("views", "./views"); //definimos la carpeta (no es necesario si la carpeta se llama views)
+
+
+// 9.7 Definir la session
+app.use(
+    session({
+        secret: "secret", //"sacret" es modificable, clave secreta para cifrar la cookie
+        resave: false, //guardar la sesión en cada solicitud
+        saveUninitialized: false, //guarda en cada peticion cuando se produzcan cambios
+    })
+);
